f3e3410543b93ab3aba1a01a2658b9f558874c2e
“Kyle <kschmit90@gmail.com>
2014-08-22 10:13:19 -0500

    Updated routes in main.rb

--- main.rb
+++ main.rb
@@ -19,71 +19,71 @@ end
 
 get '/create_user' do
 
-  erb :create_user
+  erb :'users/create_user'
 end
 
 post '/new_user' do
   @user = User.create(name: params['name'])
   @names = User.all
 
-  erb :new_user
+  erb :'users/new_user'
 end
 
 get '/user_page' do
   @user = User.find(params[:user])
 
-  erb :user_page
+  erb :'users/user_page'
 end
 
 get '/user/edit/:u_id' do
   @user = User.find(params[:u_id])
 
-  erb :edit_user
+  erb :'users/edit_user'
 end
 
 get '/user/:u_id' do
   @user = User.find(params[:u_id])
 
-  erb :user_page
+  erb :'users/user_page'
 end
 
 get '/user/:u_id/edit_recipes' do
   u_id = params[:u_id]
   @user = User.find(u_id)
 
-  erb :edit_recipes
+  erb :'recipes/edit_recipes'
 end
 
 get '/update_user/:u_id' do
   @user = User.find(params[:u_id])
   @user.update_attributes(name: params[:name])
 
-  erb :user_page
+  erb :'users/user_page'
 end
 
 get '/edit/recipe/:r_id' do
   @recipe = Recipe.find(params[:r_id])
   @categories = Category.all
 
-  erb :edit_recipe
+  erb :'recipes/edit_recipe'
 end
 
 get '/updated_recipe/:r_id' do
 
-  erb :updated_recipe
+  erb :'recipes/updated_recipe'
 end
 
 get '/user/delete/:u_id' do
   User.delete(params[:u_id])
 
-  erb :delete_user
+  erb :'users/delete_user'
 end
 
 get '/create_recipe/user/:u_id' do
   @user = User.find(params[:u_id])
   @categories = Category.all
 
-  erb :create_recipe
+  erb :'recipes/create_recipe'
 end
 
 post '/save_recipe/user/:u_id' do
@@ -105,14 +105,14 @@ post '/save_recipe/user/:u_id' do
 
   end
 
-  erb :new_recipe_page
+  erb :'recipes/new_recipe_page'
 end
 
 get '/user_recipes' do
   u_id = params['id']
   @user = User.find(u_id)
 
-  erb :user_recipes
+  erb :'users/user_recipes'
 end
 
 get '/recipe_search' do
@@ -126,12 +126,12 @@ get '/recipe_page/:id' do
   r_id = params['id']
   @recipe = Recipe.find(r_id)
 
-  erb :recipe_page
+  erb :'recipes/recipe_page'
 end
 
 get '/recipes_by_category' do
   c_id = params['category']
   @category = Category.find(c_id)
 
-  erb :category_search
+  erb :'categories/category_search'
 end
