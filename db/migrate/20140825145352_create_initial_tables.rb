c9f9faec378e145278f0449014d0fd74ac27aa49
“Kyle <kschmit90@gmail.com>
2014-08-25 10:50:45 -0500

    Wrote migration file, migrated db, tested in app, reset db with rake db:reset

--- db/migrate/20140825145352_create_initial_tables.rb
+++ db/migrate/20140825145352_create_initial_tables.rb
@@ -1,4 +1,27 @@
 class CreateInitialTables < ActiveRecord::Migration
   def change
+    create_table :categories do |t|
+      t.string :name
+    end
+    
+    create_table :ingredients do |t|
+      t.string :ingredient
+    end
+    
+    create_table :users do |t|
+      t.string :name
+    end
+    
+    create_table :recipes do |t|
+      t.string :name
+      t.text :instructions
+      t.integer :category_id
+      t.integer :user_id
+    end
+    
+    create_table :ingredients_recipes, :primary_key => false do |t|
+      t.integer :ingredient_id
+      t.integer :recipe_id
+    end
   end
 end
