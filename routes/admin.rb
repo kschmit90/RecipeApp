89d05deef03576e34dcc158e7e228708f9f49a9b
“Kyle <kschmit90@gmail.com>
2014-08-29 10:50:31 -0500

    reorganized file structure

--- routes/admin.rb
+++ routes/admin.rb
@@ -10,10 +10,22 @@ get '/admin/user_admin' do
   erb :'user_admin'
 end
 
+get '/admin/edit_user' do
+  @names = User.all
+  
+  erb :'user_edit'
+end
+
 get '/admin/recipe_admin' do
   @names = User.all
   
   erb :'recipe_admin'
+end
+
+get '/admin/edit_recipe' do
+  @names = User.all
+  
+  erb :'recipe_edit'
 end
 
 get '/admin/ingredient_admin' do
@@ -22,9 +34,21 @@ get '/admin/ingredient_admin' do
   erb :'ingredient_admin'
 end
 
+get '/admin/edit_ingredient' do
+  @names = User.all
+  
+  erb :'ingredient_edit'
+end
+
 get '/admin/category_admin' do
   @names = User.all
     
+  erb :'category_admin'
+end
+
+get '/admin/edit_category' do
+  @names = User.all
+  
   erb :'category_admin'
 end
 
