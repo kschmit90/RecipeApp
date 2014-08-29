7c6343db2a63bc93c7419bce782197cc15b44abc
“Kyle <kschmit90@gmail.com>
2014-08-29 10:54:39 -0500

    fixed view routing, changed filenames

--- routes/admin.rb
+++ routes/admin.rb
@@ -7,49 +7,49 @@ end
 get '/admin/user_admin' do
   @names = User.all
   
-  erb :'user_admin'
+  erb :'admin/user_admin/user_admin'
 end
 
 get '/admin/edit_user' do
   @names = User.all
   
-  erb :'user_edit'
+  erb :'admin/user_admin/user_edit'
 end
 
 get '/admin/recipe_admin' do
   @names = User.all
   
-  erb :'recipe_admin'
+  erb :'admin/recipe_admin/recipe_admin'
 end
 
 get '/admin/edit_recipe' do
   @names = User.all
   
-  erb :'recipe_edit'
+  erb :'admin/recipe_admin/recipe_edit'
 end
 
 get '/admin/ingredient_admin' do
   @names = User.all
   
-  erb :'ingredient_admin'
+  erb :'admin/ingredient_admin/ingredient_admin'
 end
 
 get '/admin/edit_ingredient' do
   @names = User.all
   
-  erb :'ingredient_edit'
+  erb :'admin/ingredient_admin/ingredient_edit'
 end
 
 get '/admin/category_admin' do
   @names = User.all
     
-  erb :'category_admin'
+  erb :'admin/category_admin/category_admin'
 end
 
 get '/admin/edit_category' do
   @names = User.all
   
-  erb :'category_admin'
+  erb :'admin/category_admin/category_admin'
 end
 
 
